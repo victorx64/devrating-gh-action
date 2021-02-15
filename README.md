d046f02456dec464d6de7ddb3f588c440978bcba
Victor Semenov <viktor_semenov@outlook.com>
2021-02-16 07:55:20 +0900

    Create composite action Fixes #1

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# devrating-gh-action
+This action builds a rating of contributors by the stability of code. 
+You can compete with each other and earn XP for pull requests.
+
+## Usage
+
+Just throw this to `.github/workflows/devrating.yml` in your repo:
+
+```yaml
+name: Ranked development
+on:
+  pull_request:
+    branches: [ main ]  # Put your dev branch
+    types: [ closed ]
+jobs:
+  devrating:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@v2
+      with:
+        fetch-depth: 0  # Required to be 0
+
+    - uses: victorx64/devrating-gh-action@v0
+      with:
+        devrating-organization: # Your Organization_ID. Visit https://devrating.net/#/keys to obtain
+        devrating-api-key: # Your API_Key. Visit https://devrating.net/#/keys to create new
+        base-branch: main # Put your dev branch
+```
+
+Then, merge a pull request to initiate the export.
---
+++ script.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env sh
+
+devrating_repository="$1"
+devrating_organization="$2"
+devrating_key="$3"
+base_branch="$4"
+
+send_to_devrating()
+{
+  json=$(devrating serialize commit -m $1 -p $GITHUB_WORKSPACE -l $2 -o $devrating_organization -n $devrating_repository -t $3)
+
+  set -x
+  curl -X POST "https://devrating.net/api/v1/diffs/${devrating_key}" -H "Content-Type: application/json" --data-raw $json | jq
+  set +x
+}
+
+analyze_pr()
+{
+  remainder="$1"
+  merged_at="${remainder%% *}"; remainder="${remainder#* }"
+  sha="${remainder%% *}"; remainder="${remainder#* }"
+  url="${remainder%% *}";
+
+  if [ "$merged_at" != "null" ]; then
+    send_to_devrating $sha $url $merged_at
+  fi
+}
+
+request_prs()
+{
+  prs=$(curl -H "Accept: application/vnd.github.v3+json" \
+    -H "Authorization: token ${GITHUB_TOKEN}" \
+    "https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls?base=${base_branch}&sort=updated&direction=desc&state=closed&per_page=100" | \
+    jq -c -r '.[] | "\(.merged_at) \(.merge_commit_sha) \(.html_url)"' | \
+    awk '{a[i++]=$0} END {for (j=i-1; j>=0;) print a[j--] }')
+
+  printf "$prs"
+
+  IFS=$'
+'
+
+  for pr in $prs; do
+    analyze_pr $pr
+  done
+}
+
+dotnet tool install -g devrating.consoleapp --version 3.1.1
+request_prs
+echo "Visit: https://devrating.net/#/repositories/${devrating_organization}/${devrating_repository}"
